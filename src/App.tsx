a6ae040cc872af470d1b08d8a89eaf80d2edf319
Álex Maciel <aweasley.gina@gmail.com>
2024-11-01 16:39:36 -0300

    install react router and definition routes of project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import Login from './pages/Login';
+import Dashboard from './pages/Dashboard';
+
 
 function App() {
   return (
-    <div>
-      <h1>App Financeiro Pessoal</h1>    
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Navigate to="/login" replace />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Dashboard: React.FC = () => {
+  return <div>Página de Dashboard</div>;
+};
+
+export default Dashboard;
