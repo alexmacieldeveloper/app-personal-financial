6b8658e27ef9343be102968c1f41028995b20d6e
Álex Maciel <aweasley.gina@gmail.com>
2024-11-01 18:47:56 -0300

    remove grid

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { loginUser, loginWithGoogle } from '../services/auth/authService';
-import { TextField, Button, Typography, Container, Grid, Paper, IconButton } from '@mui/material';
+import { TextField, Button, Typography, Container, Paper, IconButton } from '@mui/material';
 import WaveBackground from '../components/WaveBackground';
 import GoogleIcon from '../components/GoogleIcon';
 
