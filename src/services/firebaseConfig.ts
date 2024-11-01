e041530aa98f501b96ed6054566dbafe08fa9198
Álex Maciel <aweasley.gina@gmail.com>
2024-11-01 18:29:22 -0300

    putting firebase credentials in env

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyD8HopLV_-pGpx3vS2X8-SkZAiBsXUR1R0",
-    authDomain: "app-personal-financial.firebaseapp.com",
-    projectId: "app-personal-financial",
-    storageBucket: "app-personal-financial.firebasestorage.app",
-    messagingSenderId: "751676620359",
-    appId: "1:751676620359:web:39eeb55f226cb0ef507b62",
-    measurementId: "G-KLBC6CZJC1"
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
