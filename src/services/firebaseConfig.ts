8c7284e116704b1508fbc4df23e359cb72b6c3ef
Álex Maciel <aweasley.gina@gmail.com>
2024-11-01 21:35:28 -0300

    connect db in firebase

--- src/services/firebaseConfig.ts
+++ src/services/firebaseConfig.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,4 +13,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+export { db };
+
 export const auth = getAuth(app);
