cd735a8d6855300e852aa048694f9c5a27f0023c
Álex Maciel <aweasley.gina@gmail.com>
2024-11-01 21:19:22 -0300

    import forms

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { useAuth } from '../context/AuthContext';
+import { TextField, Button, Typography, Container, Paper, IconButton } from '@mui/material';
+import TransactionForm from '../components/TransitionForm';
 
 const Dashboard: React.FC = () => {
+  const { user } = useAuth();
     return (
-      <div>
-        <h2>Bem-vindo ao seu Dashboard</h2>
+      <Container>
+        <h2>Bem-vindo ao seu Dashboard {user?.email}</h2>
         <p>Aqui você pode visualizar e gerenciar suas finanças.</p>
-      </div>
+        <Paper style={{ padding: '24px'}}>
+          <Typography variant="h5" style={{ textAlign: 'center'}}>Adicionar transações:</Typography>
+          <TransactionForm />
+        </Paper>
+      </Container>
     );
   };
   
